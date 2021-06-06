3c2c5e466c61bf2941f5659f34d59ef8725880eb
zheng <zheng@zhengs-MacBook-Pro.local>
2021-06-05 21:51:58 -0400

    add missing scripts

--- scripts/build-env.sh
+++ scripts/build-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export MSGPACK_HOME=/Volumes/data/git-repo/gaia-bazel/submodules/msgpack-c/
+export MSGPACK_HOME=/Volumes/data/git-repo/gaia-opencv/submodules/msgpack-c/
 export MSGPACK=$MSGPACK_HOME/include
-export BOOST=/Volumes/data/git-repo/gaia-bazel/submodules/boost
+export BOOST=/Volumes/data/git-repo/gaia-opencv/submodules/boost
