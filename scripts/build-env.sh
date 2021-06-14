038a67e21d8592624ffb5d074b79f2c0cf89b1e7
zheng <zheng@zhengs-MacBook-Pro.local>
2021-06-14 12:07:13 -0400

    update env; ignore submodule unix emssdk

--- scripts/build-env.sh
+++ scripts/build-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export SUBMODULE_HOME=/Volumes/data/git-repo/gaia-opencv/submodules
+export SUBMODULE_HOME=/Volumes/data/git-repo/gaia-zbar-demo/submodules
 export MSGPACK_HOME=$SUBMODULE_HOME/msgpack-c/
 export MSGPACK=$MSGPACK_HOME/include
 export BOOST=$SUBMODULE_HOME/boost
